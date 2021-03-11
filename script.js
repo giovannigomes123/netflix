e5500f03022907d90d6f50907de5c81c91aebac9
Giovanni Gomes <giovannigomes2009@gmail.com>
2021-03-10 22:26:50 -0300

    TI-flix

---
+++ script.js
@@ -0,0 +1,15 @@
+function principal(){
+	window.open("https://www.youtube.com/watch?v=8mei6uVttho&list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV");
+}
+
+function descrever(){
+	alert("Aprenda os conceitos básicos da lógica de programação");
+}
+
+function inovar(){
+	alert("Esta parte ainda está em desenvolvimento pela equipe")
+}
+
+function cursos(){
+	alert("Este curso ainda está em desenvolvimento pela equipe");
+}
